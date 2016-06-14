e408b0f358e8bdaf6a90aba254110d0f19a631c2
Hemal Athulathmudali <hathulathmudali@virtusa.com>
2016-06-14 16:56:15 +0530

    added book detail component

---
+++ app/books/book-details.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { IBook } from './books.service';
+
+@Component({
+	moduleId: module.id,
+	selector: 'book-details',
+	styleUrls: [
+		'book-details.style.css'
+	],
+	directives: [  		
+	],
+	template: `
+		<span *ngIf="bookData !== null" class="container col-md-6">	
+			<img [src]="bookData.imageUrl" [alt]="bookData.title"/>
+			<dl>
+			  <dt>Title</dt><dd>{{ bookData.title }}</dd>
+			  <dt>Author</dt><dd>{{ bookData.author }}</dd>
+			  <dt>Price</dt><dd>{{ bookData.price }}</dd>
+			</dl>
+			<button class="btn btn-success">Purchase</button>				
+		</span>
+		<span *ngIf="bookData === null" class="col-md-6">
+			<br/>
+			<span class="well well-lg">Select book to view details...</span>	
+		</span>
+	`
+})
+export class BookDetailsComponent implements OnInit { 
+
+  	@Input() bookData: IBook;  	
+
+	constructor(){
+
+	}
+
+	ngOnInit(){
+		
+	}
+}
